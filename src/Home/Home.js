7e6a487e0b1a12dc8259b5ef717f555099371a8b
Ankit Singh <aks.11feb@gmail.com>
2021-03-02 18:10:28 +0530

    timer is added

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -10,10 +10,86 @@ import "./Home.css";
 import axios from "axios";
 import { baseUrl } from "../shared/baseUrl";
 import { Formik, Form } from "formik";
+import { makeStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+import PropTypes from 'prop-types';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+function CircularProgressWithLabel(props) {
+  return (
+    <Box position="relative" display="inline-flex">
+      <CircularProgress variant="determinate" {...props} />
+      <Box
+        top={0}
+        left={0}
+        bottom={0}
+        right={0}
+        position="absolute"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Typography variant="caption" component="div" color="textSecondary">{`${Math.round(
+          props.value,
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+});
+
+
+CircularProgressWithLabel.propTypes = {
+  /**
+   * The value of the progress indicator for the determinate variant.
+   * Value between 0 and 100.
+   */
+  value: 100,
+};
+
+
 
 var result = new Array();
 
 function Home(props) {
+  const [page, setPage] = useState(0);
+
+  const [progress, setProgress] = React.useState(0);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) => {
+        if (prevProgress <= 0) {
+
+          setPage(page => page + 1)
+          return 100;
+        }
+        else {
+          return prevProgress - 10;
+        }
+      })
+
+      // (prevProgress <= 0 ? 100 : prevProgress - 10));
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [page]);
+
+
+
+  const classes = useStyles();
+
+
   const accessToken = `${props.login?.login?.data?.token}`;
 
   const authAxios = axios.create({
@@ -31,7 +107,7 @@ function Home(props) {
   const [category, setCategory] = useState([]);
   const [user, setUser] = useState([]);
 
-  const [page, setPage] = useState(0);
+
 
   const [value, setValue] = React.useState();
   const [counter, setCounter] = React.useState(0);
@@ -54,6 +130,7 @@ function Home(props) {
         setOption(res.data);
         if (res) {
           setCounter(10);
+          setProgress(100);
         }
       })
       .catch((err) => console.log(err));
@@ -96,6 +173,7 @@ function Home(props) {
     setPage(page + 1);
     setCounter(10);
     setValue();
+    setProgress(100)
   };
 
   const answer = () => {
@@ -121,6 +199,16 @@ function Home(props) {
       })
       .catch((err) => console.log(err));
   };
+
+  // React.useEffect(() => {
+
+  //   const timer = setInterval(() => {
+  //     setPage(page => page + 1)
+  //   }, 10000);
+  //   return () => {
+  //     clearInterval(timer);
+  //   };
+  // }, [page]);
 
   if (props.login?.login.length === 0) {
     return <Redirect to={"/login"} />;
@@ -153,10 +241,22 @@ function Home(props) {
               {question.length !== 0 ? (
                 <Card className="question">
                   <CardHeader>
+                    <div className={classes.root}>
+                      <LinearProgress variant="determinate" value={progress} />
+                    </div>
+                  </CardHeader>
+                  {/* <CardHeader>
                     <strong style={{ textTransform: "capitalize" }}>
                       {question?.data[page]?.major_category?.name}
                     </strong>
-                    {/* <p className="pull-right text-red">{counter}</p> */}
+                    <p className="pull-right text-red">{counter}</p>
+                  </CardHeader> */}
+                  <CardHeader style={{ display: "flex", justifyContent: "space-between" }}>
+                    <h6 style={{ fontSize: "14px" }}>Question No.{"  "}{page + 1}</h6>
+                    <div className="float-right">
+
+                      {/* <CircularProgressWithLabel value={progress} /> */}
+                    </div>
                   </CardHeader>
                   <CardHeader
                     style={{
@@ -166,26 +266,27 @@ function Home(props) {
                     }}
                   >
                     {/* <h6>Instructions:</h6> */}
-                    <span className="ml-1">
+                    <h6 className="" style={{ textTransform: "uppercase" }}>
                       {question?.data[page]?.instructions}
-                    </span>
+                    </h6>
                   </CardHeader>
-                  <CardBody>
-                    <h6>Question No.{"  "}{page + 1}</h6>
+                  <CardBody style={{ textTransform: "uppercase" }}>
+
 
                     <div className="mb-2">
 
-                      <h6>{question?.data[page].question_text} ?</h6>
+                      <h6>{question?.data[page]?.question_text} ?</h6>
 
                       {option?.data?.map((opt, ind) => {
-                        if (question?.data[page].id == opt.question_id)
+                        if (question?.data[page]?.id == opt.question_id)
                           return (
                             <div key={ind}>
                               <input
                                 type="radio"
                                 className="mr-2"
                                 key={opt.question_id}
-                                name="total_points"
+                                name="
+                                "
                                 onChange={() => setValue(opt.id)}
                                 value={opt.id && opt.points}
                               />
